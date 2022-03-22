2832c8da5410c8bff827582bdf762c0f3bc37b6f
Dgo45921 <3023135080101@ingenieria.edu.gt>
2022-03-22 11:15:04 -0600

    cambio en nodo

--- NodoRobot.py
+++ NodoRobot.py
@@ -1,7 +1,5 @@
 class NodoRobot:
-    def __init__(self, x=None, y=None, robot=None, siguiente=None, anterior=None):
-        self.x = x
-        self.y = y
+    def __init__(self, robot=None, siguiente=None, anterior=None):
         self.robot = robot
         self.siguiente = siguiente
         self.anterior = anterior
