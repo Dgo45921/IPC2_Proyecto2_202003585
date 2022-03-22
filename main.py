7c7c7e9401316c6f2209ee012d18f3d845a18f95
Dgo45921 <3023135080101@ingenieria.edu.gt>
2022-03-21 18:24:36 -0600

    capaz de leer xml

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import ManejoXML
 
 
 def main():
-    print("---------MENU---------")
-    print("")
+    while True:
+        print("Por favor ingrese una opción")
+        print("---------MENU---------")
+        print("1.Cargar xml\n2.Ver ciudades\n3.Sistema de control\n4.Salir")
+        opcion = input()
+        if opcion == "1":
+            print("Ingrese la ruta del xml:")
+            ruta = input()
+            ManejoXML.leer_xml(ruta)
+
+        elif opcion == "2":
+            print("ver las ciudades")
+        elif opcion == "3":
+            print("sistema de control")
+        elif opcion == "4":
+            print("Salir")
+            break
+        else:
+            print("Por favor, ingrese una opción válida")
+
+
+main()
